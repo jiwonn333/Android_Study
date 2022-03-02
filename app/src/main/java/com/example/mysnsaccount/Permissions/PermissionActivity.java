19afba985b9a295308c3ac77eb62ab9e3d61c13b
jiwon <jiwon3378@dkitec.com>
2022-03-02 16:07:23 +0900

    [OJT_permission] permission 코드 수정 commit

--- app/src/main/java/com/example/mysnsaccount/Permissions/PermissionActivity.java
+++ app/src/main/java/com/example/mysnsaccount/Permissions/PermissionActivity.java
@@ -1,7 +1,6 @@
 package com.example.mysnsaccount.Permissions;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -44,19 +43,17 @@ public class PermissionActivity extends AppCompatActivity {
                     TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
 
                     //READ_PHONE_NUMBERS와 READ_PRECISE_PHONE_STATE 권한 허가 받았는지 확인
-//                    if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED
-//                            && ContextCompat.checkSelfPermission(context, Manifest.permission.READ_PRECISE_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-//                        return;
-//                    }
+                    if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED
+                            && ContextCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+
                     Log.d(TAG, "음성통화 상태 : [ getCallState ] >>> " + tm.getCallState());
                     Log.d(TAG, "데이터통신 상태 : [ getDataState ] >>> " + tm.getDataState());
                     Log.d(TAG, "통신사 ISO 국가코드 : [ getNetworkCountryIso ] >>> " + tm.getNetworkCountryIso());
                     Log.d(TAG, "통신사 ISO 국가코드 : [ getSimCountryIso ] >>> " + tm.getSimCountryIso());
                     Log.d(TAG, "SIM 카드 상태 : [ getSimState ] >>> " + tm.getSimState());
 
-//                    @SuppressLint("HardwareIds") String android_id = Settings.Secure.getString(this.getContentResolver(),
-//                            Settings.Secure.ANDROID_ID);
-//                    Log.d(TAG, "Android_ID >>> " + android_id);
                 }
             }
         });
@@ -67,21 +64,19 @@ public class PermissionActivity extends AppCompatActivity {
         boolean mPermissionsGranted = false;
         String[] mRequiredPermissions = new String[1];
 
-        mRequiredPermissions[0] = Manifest.permission.READ_PRECISE_PHONE_STATE;
-
         //안드로이드 버전에 따라 권한 다르게 요청
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-//            //11이상인 경우
-//            mRequiredPermissions[0] = Manifest.permission.READ_PHONE_NUMBERS;
-//        } else {
-//            //10이하인 경우
-//            mRequiredPermissions[0] = Manifest.permission.READ_PRECISE_PHONE_STATE;
-//        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            //11이상인 경우
+            mRequiredPermissions[0] = Manifest.permission.READ_PHONE_NUMBERS;
+        } else {
+            //10이하인 경우
+            mRequiredPermissions[0] = Manifest.permission.READ_PHONE_STATE;
+        }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             //필수 권한을 가지고 있는지 확인
             mPermissionsGranted = hasPermissions(mRequiredPermissions);
-            Log.d("sjw", "grantResults mPermissionsGranted "+ mPermissionsGranted);
+            Log.d("sjw", "grantResults mPermissionsGranted " + mPermissionsGranted);
             //필수 권한 중 한 개라도 없는 경우
             if (!mPermissionsGranted) {
                 //권한을 요청한다.
@@ -114,53 +109,35 @@ public class PermissionActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == PERMISSION_REQUEST_CODE) {
-            //권한을 모두 승인했는지 여부
-            boolean chkFlag = false;
-            //승인한 권한은 0값, 승인 안한 권한은 -1을 값으로 가짐
-            Log.d("sjw", "grantResults length "+grantResults.length);
-            for (int g : grantResults) {
-                Log.d("sjw", "grantResults g "+g);
-                if (g == -1) {
-                    chkFlag = true;
-                    break;
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    /*권한이 있는경우 실행할 코드....*/
+                    logic();
+                } else {
+                    // 하나라도 거부한다면.
+                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+                    alertDialog.setTitle("앱 권한");
+                    alertDialog.setMessage("해당 앱의 원할한 기능을 이용하시려면 애플리케이션 정보>권한> 에서 모든 권한을 허용해 주십시오");
+                    // 권한설정 클릭시 이벤트 발생
+                    alertDialog.setPositiveButton("권한설정",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+                                    startActivity(intent);
+                                    dialog.cancel();
+                                }
+                            });
+                    //취소
+                    alertDialog.setNegativeButton("취소",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            });
+                    alertDialog.show();
                 }
-            }
-            //권한 중 한개라도 승인 안 한 경우
-//            if (chkFlag) {
-//                checkPermission();
-//            }
-
-            switch (requestCode) {
-                case PERMISSION_REQUEST_CODE:
-                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                        /*권한이 있는경우 실행할 코드....*/
-                        logic();
-                    } else {
-                        // 하나라도 거부한다면.
-                        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-                        alertDialog.setTitle("앱 권한");
-                        alertDialog.setMessage("해당 앱의 원할한 기능을 이용하시려면 애플리케이션 정보>권한> 에서 모든 권한을 허용해 주십시오");
-                        // 권한설정 클릭시 이벤트 발생
-                        alertDialog.setPositiveButton("권한설정",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
-                                        startActivity(intent);
-                                        dialog.cancel();
-                                    }
-                                });
-                        //취소
-                        alertDialog.setNegativeButton("취소",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.cancel();
-                                    }
-                                });
-                        alertDialog.show();
-                    }
-                    break;
-            }
+                break;
         }
     }
 }
